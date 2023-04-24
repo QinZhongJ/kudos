c61d3640ad77c1b49168a8296ffb29d86a5334b3
QinZhongjie <571489624@qq.com>
2023-04-24 15:21:32 +0800

    Initial Commmit

---
+++ app/routes/index.ts
@@ -0,0 +1,9 @@
+// ./app/routes/index.tsx
+import type { LoaderFunction } from '@remix-run/node';
+import { redirect } from '@remix-run/node';
+import { requireUserId } from '~/utils/auth.server';
+
+export const loader: LoaderFunction = async ({ request }) => {
+  await requireUserId(request);
+  return redirect('/home');
+};
